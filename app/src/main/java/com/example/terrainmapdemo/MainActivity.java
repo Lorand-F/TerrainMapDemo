e478eaaf70326199a5218c9cf60aec93cf59f195
Lorand-F <formagiu.lorand@gmail.com>
2022-10-25 20:40:41 +0300

    Phase 2: Java. There was a problem with FragmentActivity, an update to a specific library in gradle.app and so on. The project was succsefully implemented.

--- app/src/main/java/com/example/terrainmapdemo/MainActivity.java
+++ app/src/main/java/com/example/terrainmapdemo/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.terrainmapdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap googleMap;
+    TextView tvNormal, tvTerrain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvNormal=findViewById(R.id.tv_normal);
+        tvTerrain=findViewById(R.id.tv_terrain);
+
+        tvTerrain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                tvTerrain.setVisibility(View.GONE);
+                tvNormal.setVisibility(View.VISIBLE);
+            }
+        });
+
+        tvNormal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                tvNormal.setVisibility(View.GONE);
+                tvTerrain.setVisibility(View.VISIBLE);
+            }
+        });
+
+        SupportMapFragment mapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap=map;
+
     }
 }
